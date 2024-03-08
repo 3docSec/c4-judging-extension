2f8b0ca8339d624c767b9abc6c9fc06b77b36dc7
3doc <3docsec@users.noreply.github.com>
2024-03-08 15:05:00 +0100

    Hack with code lens provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
+import { CodelensProvider } from './CodelensProvider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "c4-judging" is now active!');
+let disposables: Disposable[] = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('c4-judging.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from c4-judging!');
+export function activate(context: ExtensionContext) {
+	const codelensProvider = new CodelensProvider();
+
+	languages.registerCodeLensProvider("*", codelensProvider);
+
+	commands.registerCommand("codelens-sample.enableCodeLens", () => {
+		workspace.getConfiguration("codelens-sample").update("enableCodeLens", true, true);
 	});
 
-	context.subscriptions.push(disposable);
+	commands.registerCommand("codelens-sample.disableCodeLens", () => {
+		workspace.getConfiguration("codelens-sample").update("enableCodeLens", false, true);
+	});
+
+	commands.registerCommand("codelens-sample.codelensAction", (args: any) => {
+		window.showInformationMessage(`CodeLens action clicked with args=${args}`);
+	});
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// this method is called when your extension is deactivated
+export function deactivate() {
+	if (disposables) {
+		disposables.forEach(item => item.dispose());
+	}
+	disposables = [];
+}
